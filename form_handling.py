9b61ff65ea78ca412b6cf5578bffcc4d06dd4d4b
absiddik7 <abubakkar1070@gmail.com>
2023-09-22 12:28:30 +0600

    Form handling updated

--- form_handling.py
+++ form_handling.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 import time
 
-
 captcha_list = []
+url_list = []
 
 # Read the Test input contact form.csv file
 form_input_data = pd.read_csv('Test input contact form.csv')
@@ -24,49 +24,39 @@ subject = form_input_data['Subject']
 message = form_input_data['Message']
 
 
-def url_file_read():
-    my_urls = []
-    with open('successful_urls.csv', 'r') as file:
-        data = csv.reader(file)
-        for csv_data in data:
-            my_urls.append(csv_data[0].lstrip(" "))
-    return my_urls
-
-
+# def submit_contact_form():
 def submit_contact_form():
     option = webdriver.FirefoxOptions()
-    option.add_argument('--no-sandbox')
-    option.add_argument('--disable-dev-shm-usage')
-    # Enable headless mode
-    # option.add_argument('--headless')
-    # Initialize the WebDriver with the configured options
     driver = webdriver.Firefox(options=option)
     try:
-        driver.get("https://www.tricityrentals.com/contactus.aspx")
-        time.sleep(10)
+        driver.get("https://251-dekalb0-rentcafewebsite.securecafe.com/onlineleasing/apartmentsforrent/contactus")
+        time.sleep(5)
 
         # Find and fill out the form fields if they exist
-
         # Full Name
         try:
             full_name_field = driver.find_element(By.XPATH, "//input["
                                                             "@name='your-name' or "
                                                             "@name='full-name' or "
                                                             "@name='fullname' or"
+                                                            "@name='name' or"
                                                             "@placeholder='Full Name'"
                                                             "]")
             full_name_field.send_keys(first_name[0] + " " + last_name[0])
         except NoSuchElementException:
             pass
+
         # First Name
         try:
             first_name_field = driver.find_element(By.XPATH, "//input["
                                                              "@name='first-name' or "
+                                                             "@name='first_name' or"
                                                              "@name='fname' or "
                                                              "@name='firstname' or"
                                                              "@name='Firstname' or"
                                                              "@name='FirstName' or"
                                                              "@placeholder='First Name' or"
+                                                             "@placeholder='First Name*' or"
                                                              "@placeholder='First'"
                                                              "]")
             first_name_field.send_keys(first_name[0])
@@ -76,11 +66,13 @@ def submit_contact_form():
         try:
             last_name_field = driver.find_element(By.XPATH, "//input["
                                                             "@name='last-name' or"
+                                                            "@name='last_name' or"
                                                             " @name='lname' or"
                                                             " @name='lastname' or"
                                                             " @name='Lastname' or"
                                                             " @name='LastName' or"
                                                             " @placeholder='Last Name' or"
+                                                            " @placeholder='Last Name*' or"
                                                             " @placeholder='Last'"
                                                             "]")
             last_name_field.send_keys(last_name[0])
@@ -95,9 +87,23 @@ def submit_contact_form():
 
         # Telephone Number
         try:
-            tel_phone_field = driver.find_element(By.XPATH, "//input[@type='tel' or @inputmode='numeric'] |"
-                                                            "//span[contains(text(),'Phone') or contains(text(),'Phone Number') or contains(text(),'Telephone')]/following::input[@type='text'] | "
-                                                            "//label[contains(text(),'Phone' ) or contains(text(),'Phone Number') or contains(text(),'Telephone')]/following::input[@type='text']")
+            tel_phone_field = driver.find_element(By.XPATH, "//input[@type='tel' or "
+                                                            "@inputmode='numeric' or "
+                                                            "@name='phone' or "
+                                                            "@name='phone_number' or "
+                                                            "@name='phone-number' or "
+                                                            "@name='telephone' or "
+                                                            "@placeholder='Phone', or "
+                                                            "@placeholder='Phone No' or "
+                                                            "@placeholder='Phone No*' or "
+                                                            "@placeholder='Phone No.' or "
+                                                            "@placeholder='Telephone'] |"
+                                                            "//span[contains(text(),'Phone') or "
+                                                            "contains(text(),'Phone Number') or "
+                                                            "contains(text(),'Telephone')]/following::input[@type='text'] | "
+                                                            "//label[contains(text(),'Phone' ) or "
+                                                            "contains(text(),'Phone Number') or "
+                                                            "contains(text(),'Telephone')]/following::input[@type='text']")
             tel_phone_field.send_keys(tel_phone[0])
         except NoSuchElementException:
             pass
@@ -135,9 +141,20 @@ def submit_contact_form():
 
         # Email
         try:
-            email_field = driver.find_element(By.XPATH, "//input[@type='email'] |"
-                                                        "//span[contains(text(),'Email') or contains(text(),'Email Address')]/following::input[@type='text'] |"
-                                                        "//label[contains(text(),'Email') or contains(text(),'Email Address')]/following::input[@type='text']")
+            email_field = driver.find_element(By.XPATH, "//input[@type='email' or"
+                                                        " @name='email' or"
+                                                        " @name='Email' or"
+                                                        " @name='email_address' or "
+                                                        "@name='email-address' or "
+                                                        "@placeholder='email' or "
+                                                        "@placeholder='Email' or "
+                                                        "@placeholder='Email*' or "
+                                                        "@placeholder='Your Email' or "
+                                                        "@placeholder='Email Address'] |"
+                                                        "//span[contains(text(),'Email') or "
+                                                        "contains(text(),'Email Address')]/following::input[@type='text'] |"
+                                                        "//label[contains(text(),'Email') or "
+                                                        "contains(text(),'Email Address')]/following::input[@type='text']")
             email_field.send_keys(email[0])
         except NoSuchElementException:
             pass
@@ -147,22 +164,58 @@ def submit_contact_form():
             message_field = driver.find_element(By.TAG_NAME, "textarea")
             message_field.send_keys(message[0])
         except NoSuchElementException:
-
             pass
 
-        time.sleep(25)
+        time.sleep(5)
         driver.quit()
 
-        time.sleep(5)
     except WebDriverException as e:
         driver.quit()
+
+
+def open_link_in_one_browser(urls):
+    option = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(options=option)
+
+    # Implicit wait
+    driver.implicitly_wait(5)
+
+    # Loop through the list of links
+    for url in urls:
+        # Switch to the new tab
+        driver.switch_to.window(driver.window_handles[-1])
+
+        # Navigate to the link
+        driver.get(url)
         time.sleep(5)
+
+    # Store window handle IDs
+    window_handles = driver.window_handles
+
+    # Iterate through tabs
+    for handle in window_handles:
+        driver.switch_to.window(handle)
+
+    # Quit the browser
+    driver.quit()
 
 
 if __name__ == '__main__':
-    # urls = url_file_read()
+    # data = pd.read_csv("successful_urls.csv", header=None)
+    # url_list = data.values.flatten().tolist()
+    # print(url_list)
+    # links = ["https://www.google.com", "https://www.youtube.com", "https://www.netflix.com"]
+    # print(links)
+    # open_link_in_one_browser(url_list)
+
     submit_contact_form()
 
+    # urls = url_file_read()
     # for index, url in enumerate(urls):
     #     print(index, url)
-    # submit_contact_form(url)
+    #     te(url)
+
+    #     submit_contact_form(url)
+    # te()
+
+    # submit_contact_form()
